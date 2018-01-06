3fdd9beeaacf3cf0d5928de3a907c7afb7c968db
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2018-01-06 16:16:01 -0600

    Streamline and update to latest Firebase version

--- app/src/main/java/com/bubenheimer/firedance/MainActivity.java
+++ app/src/main/java/com/bubenheimer/firedance/MainActivity.java
@@ -5,9 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public final class MainActivity extends Activity {
+//    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        final String iid = FirebaseInstanceId.getInstance().getId();
+//        Log.i(TAG, "FirebaseInstanceId.getInstance().getId() == " + iid);
+
         startService(new Intent(this, FBService.class));
+
+        finish();
     }
 }
